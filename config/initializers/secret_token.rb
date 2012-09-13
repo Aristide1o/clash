3accbaf628bbdc501a9c5cc14c735efae5d5c41c
Aristide Niyungeko <niyungeko@gmail.com>
2012-09-13 02:07:29 -0700

    Commit everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clash::Application.config.secret_token = '588730cf1d63a0ff327554f30c0a39998c5039ae450c1fa3f7236f840a43aaa1f469376eb3b612f218f20d18e6a0f8fd251065f2e4f62727fc7e7164e8f10f1a'
